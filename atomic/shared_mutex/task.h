9116d1b5f5fac3fca32e7f45f4b8b712247f1262
Bolat Idrisov <bolatusd@yandex.ru>
2023-03-30 19:09:51 +0000

    solution passed tests locally

--- atomic/shared_mutex/task.h
+++ atomic/shared_mutex/task.h
@@ -1,16 +1,61 @@
 #pragma once
+#include <atomic>
+
+
+class SimpleMutex {
+   public:
+    SimpleMutex() = default;
+
+    void lock() {
+        while (flag.test_and_set(std::memory_order_acquire)) {
+            __builtin_ia32_pause();
+        }
+    }
+
+    void unlock() { flag.clear(std::memory_order_release); }
+
+   private:
+    std::atomic_flag flag = ATOMIC_FLAG_INIT;
+};
 
 class SharedMutex {
 public:
-    void lock() {
-    }
-
-    void unlock() {
-    }
-
     void lock_shared() {
+        mutex_.lock();
+        while (uniq_lck_) {
+            mutex_.unlock();
+            __builtin_ia32_pause();
+            mutex_.lock();
+        }
+        shared_cnt_ += 1;
+        mutex_.unlock();
     }
 
     void unlock_shared() {
+        mutex_.lock();
+        shared_cnt_ -= 1;
+        mutex_.unlock();
     }
+
+    void lock() {
+        mutex_.lock();
+        while (shared_cnt_ != 0 || uniq_lck_) {
+            mutex_.unlock();
+            __builtin_ia32_pause();
+            mutex_.lock();
+        }
+        uniq_lck_ = true;
+        mutex_.unlock();
+    }
+
+    void unlock() {
+        mutex_.lock();
+        uniq_lck_ = false;
+        mutex_.unlock();
+    }
+
+   private:
+    int shared_cnt_ = 0;
+    bool uniq_lck_ = false;
+    SimpleMutex mutex_;
 };
