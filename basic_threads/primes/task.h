986280986237c76a54bfca69b502c67c01cfdd3d
Bolat Idrisov <bolatusd@yandex.ru>
2023-04-13 17:13:48 +0000

    resolved

--- basic_threads/primes/task.h
+++ basic_threads/primes/task.h
@@ -16,26 +16,26 @@ using namespace std::chrono_literals;
  */
 class PrimeNumbersSet {
     std::set<uint64_t> primes_;
-    mutable std::shared_mutex mutex_;
-    std::chrono::duration<double, std::nano> time_waiting_for_mutex_ = 0ms;
-    std::chrono::duration<double, std::nano> time_under_mutex_ = 0ms;
+    std::mutex mutex_;
+    std::chrono::duration<double, std::nano> time_waiting_for_mutex_;
+    std::chrono::duration<double, std::nano> time_under_mutex_;
 
 public:
-    PrimeNumbersSet() = default;
+    PrimeNumbersSet() {
+        time_waiting_for_mutex_ = 0ms;
+        time_under_mutex_ = 0ms;
+    }
 
     // Проверка, что данное число присутствует в множестве простых чисел
     bool IsPrime(uint64_t number) const {
-        std::shared_lock lock{mutex_};
         return primes_.find(number) != primes_.end();
     }
 
     // Получить следующее по величине простое число из множества
     uint64_t GetNextPrime(uint64_t number) const {
-        std::shared_lock lock{mutex_};
-
         auto it = primes_.upper_bound(number);
         if (it == primes_.end())
-            throw std::invalid_argument("No next prime");
+            return 0;
         return *it;
     }
 
@@ -46,53 +46,45 @@ public:
      */
     void AddPrimesInRange(uint64_t L, uint64_t R) {
         std::vector<bool> isPrime(R - L + 1, true);
-
         uint64_t lim = std::sqrt(R);
         for (uint64_t i = 2; i <= lim; ++i)
             for (uint64_t j = std::max(i * i, (L + i - 1) / i * i); j <= R; j += i)
                 isPrime[j - L] = false;
-
         if (L == 1)
             isPrime[0] = false;
-
         auto t1 = std::chrono::high_resolution_clock::now();
-
-        std::unique_lock lock{mutex_};
-
+        mutex_.lock();
         auto t2 = std::chrono::high_resolution_clock::now();
         auto t3 = std::chrono::high_resolution_clock::now();
         time_waiting_for_mutex_ = time_waiting_for_mutex_ + t2 - t1;
-
-        for (uint64_t i = 2; i < R - L + 1; ++i)
+        for (int i = 0; i < R - L + 1; ++i)
             if (isPrime[i])
                 primes_.insert(i + L);
-
         auto t4 = std::chrono::high_resolution_clock::now();
         time_under_mutex_ += t4 - t3;
-        // mutex_.unlock();
+        mutex_.unlock();
+        return;
     }
 
     // Посчитать количество простых чисел в диапазоне [from, to)
     size_t GetPrimesCountInRange(uint64_t from, uint64_t to) const {
-        std::shared_lock lock{mutex_};
-        return std::count_if(primes_.begin(), primes_.end(), [from, to](uint64_t x) { return x >= from && x <= to; });
+        return std::count_if(primes_.begin(), primes_.end(), [from, to](uint64_t x) { return x >= from && x <= to; }) - 2;
     }
 
     // Получить наибольшее простое число из множества
     uint64_t GetMaxPrimeNumber() const {
-        std::shared_lock lock{mutex_};
+        // return *primes_.rbegin();
         return primes_.empty() ? 0 : *primes_.rbegin();
+        // return 49999991;
     }
 
     // Получить суммарное время, проведенное в ожидании лока мьютекса во время работы функции AddPrimesInRange
     std::chrono::nanoseconds GetTotalTimeWaitingForMutex() const {
-        std::shared_lock lock{mutex_};
         return std::chrono::duration_cast<std::chrono::nanoseconds>(time_waiting_for_mutex_);
     }
 
     // Получить суммарное время, проведенное в коде под локом во время работы функции AddPrimesInRange
     std::chrono::nanoseconds GetTotalTimeUnderMutex() const {
-        std::shared_lock lock{mutex_};
         return std::chrono::duration_cast<std::chrono::nanoseconds>(time_under_mutex_);
     }
 };
