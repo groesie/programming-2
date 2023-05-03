65f22b2493e8e7a4c1463ce6984d4bcd2ed828bf
mthgradr <bolatusd@yandex.ru>
2023-05-03 12:46:20 +0300

    list enhanced

--- python-basics/enhanced_list/enhanced_list.py
+++ python-basics/enhanced_list/enhanced_list.py
@@ -9,4 +9,33 @@ class EnhancedList(list):
         - если новая длина больше старой, то дополнить список значениями None;
         - если новая длина меньше старой, то удалять значения из конца списка.
     """
-    pass
+    @property
+    def first(self):
+        return self[0]
+    
+    @first.setter
+    def first(self, val):
+        self[0] = val
+
+    @property
+    def last(self):
+        return self[-1]
+    
+    @last.setter
+    def last(self, val):
+        self[-1] = val
+    
+    @property
+    def size(self):
+        return self.__len__()
+    
+    @size.setter
+    def size(self, val):
+        d = val - self.size
+        if d < 0:
+            del self[d:]
+        else:
+            self += [None] * (d)
+            
+    
+    
