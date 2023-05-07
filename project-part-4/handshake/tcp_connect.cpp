61f40f253d1adea93aa2b26e614ef3592e88a511
mthgradr <bolatusd@yandex.ru>
2023-05-07 22:55:01 +0300

    establishment fixed

--- project-part-4/handshake/tcp_connect.cpp
+++ project-part-4/handshake/tcp_connect.cpp
@@ -18,42 +18,86 @@ TcpConnect::~TcpConnect() {
 }
 
 void TcpConnect::EstablishConnection() {
+    // sock_ = socket(AF_INET, SOCK_STREAM, 0);
+    // if (sock_ < 0) {
+    //     throw std::runtime_error(std::string("Error creating socket: ") + strerror(errno));
+    // }
+
+    // struct sockaddr_in addr;
+    // addr.sin_family = AF_INET;
+    // addr.sin_port = htons(port_);
+    // inet_pton(AF_INET, ip_.c_str(), &addr.sin_addr);
+
+    // int flags = fcntl(sock_, F_GETFL, 0);
+    // fcntl(sock_, F_SETFL, flags | O_NONBLOCK);
+
+    // int res = connect(sock_, (struct sockaddr*)&addr, sizeof(addr));
+    // if (res < 0 && errno != EINPROGRESS) {
+    //     throw std::runtime_error(std::string("Error connecting to the server: ") + strerror(errno));
+    // }
+
+    // struct pollfd pfd;
+    // pfd.fd = sock_;
+    // pfd.events = POLLOUT;
+
+    // int poll_res = poll(&pfd, 1, connectTimeout_.count());
+    // if (poll_res <= 0) {
+    //     CloseConnection();
+    //     throw std::runtime_error("Connection timeout");
+    // }
+
+    // fcntl(sock_, F_SETFL, flags);
+
+    // int error = 0;
+    // socklen_t error_len = sizeof(error);
+    // getsockopt(sock_, SOL_SOCKET, SO_ERROR, &error, &error_len);
+    // if (error != 0) {
+    //     throw std::runtime_error(std::string("Error during connection: ") + strerror(error));
+    // }
     sock_ = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_ < 0) {
-        throw std::runtime_error(std::string("Error creating socket: ") + strerror(errno));
+    if (sock_ == -1)
+        throw std::runtime_error("Can't create a socket!\n");
+
+    struct sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port_);
+    if(inet_pton(AF_INET, ip_.c_str(), &hint.sin_addr) <= 0)
+        throw std::runtime_error("Invalid IP address\n");
+
+    int isOn = 1;
+    setsockopt(sock_, SOL_SOCKET, SO_REUSEADDR, &isOn, sizeof(int));
+
+    if (fcntl(sock_, F_SETFL, O_NONBLOCK) == -1)
+        throw std::runtime_error("Cannot set to nonblocking mode\n");
+
+    if (connect(sock_, (struct sockaddr *) &hint, sizeof(hint)) == -1) {
+        if (errno != EINPROGRESS) {
+           throw std::runtime_error("Can't connect to IP/port\n");
+        }
     }
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port_);
-    inet_pton(AF_INET, ip_.c_str(), &addr.sin_addr);
+    fd_set fdSet;
+    FD_ZERO(&fdSet);
+    FD_SET(sock_, &fdSet);
 
-    int flags = fcntl(sock_, F_GETFL, 0);
-    fcntl(sock_, F_SETFL, flags | O_NONBLOCK);
+    struct timeval tv;
+    tv.tv_usec = connectTimeout_.count();
 
-    int res = connect(sock_, (struct sockaddr*)&addr, sizeof(addr));
-    if (res < 0 && errno != EINPROGRESS) {
-        throw std::runtime_error(std::string("Error connecting to the server: ") + strerror(errno));
+    if (select(sock_ + 1, NULL, &fdSet, NULL, &tv) == 1)
+    {
+        int so_error;
+        socklen_t length = sizeof(so_error);
+
+        getsockopt(sock_, SOL_SOCKET, SO_ERROR, &so_error, &length);
+
+        if (so_error == 0) {
+            int fgs = fcntl(sock_, F_GETFL, 0);
+            fgs = fgs & ~O_NONBLOCK;
+            fcntl(sock_, F_SETFL, fgs);
+            return;
+        }
     }
-
-    struct pollfd pfd;
-    pfd.fd = sock_;
-    pfd.events = POLLOUT;
-
-    int poll_res = poll(&pfd, 1, connectTimeout_.count());
-    if (poll_res <= 0) {
-        CloseConnection();
-        throw std::runtime_error("Connection timeout");
-    }
-
-    fcntl(sock_, F_SETFL, flags);
-
-    int error = 0;
-    socklen_t error_len = sizeof(error);
-    getsockopt(sock_, SOL_SOCKET, SO_ERROR, &error, &error_len);
-    if (error != 0) {
-        throw std::runtime_error(std::string("Error during connection: ") + strerror(error));
-    }
+    throw std::runtime_error("Timeout exceeded\n");
 }
 
 void TcpConnect::SendData(const std::string& data) const {
