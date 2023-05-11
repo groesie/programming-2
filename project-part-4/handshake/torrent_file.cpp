7d51b9c43a257529dbfcbb4bcfcd9b4862ebc04e
mthgradr <bolatusd@yandex.ru>
2023-05-11 21:17:39 +0300

    fixed

--- project-part-4/handshake/torrent_file.cpp
+++ project-part-4/handshake/torrent_file.cpp
@@ -1,63 +1,49 @@
 #include "torrent_file.h"
 #include "bencode.h"
-#include <vector>
-#include <openssl/sha.h>
-#include <fstream>
+#include "byte_tools.h"
 #include <variant>
-#include <sstream>
-
-#include <string>
-
-
 
 TorrentFile LoadTorrentFile(const std::string& filename) {
-    std::ifstream tifstream(filename);
-    std::stringstream tstream;
-  
-    tstream << tifstream.rdbuf();    
-    tifstream.close();
-   
-    tstream.unsetf(std::ios_base::skipws);
-    tstream.seekg(std::ios::beg);
-    std::shared_ptr<Bencode::TorElement> root = Bencode::getRoot(tstream);
+    TorrentFile torrentFile;
 
-    TorrentFile tfile;
+    std::ifstream file(filename);
+    std::stringstream fileStringStream;
+    fileStringStream << file.rdbuf();
 
-    std::unordered_map<std::string, std::shared_ptr<Bencode::TorElement>> d = std::get<1>(root->value);
-    tfile.announce = std::get<0>(d["announce"]->value);
-    tfile.comment = std::get<0>(d["comment"]->value);
+    std::string torrentString = fileStringStream.str();
 
-    std::unordered_map<std::string, std::shared_ptr<Bencode::TorElement>> d_info = std::get<1>(d["info"]->value);
+    Bencode::Decoder decoder(torrentString);
 
-    tfile.name = std::get<0>(d_info["name"]->value);
-    tfile.length = std::get<3>(d_info["length"]->value);
-    std::vector<std::string> pieceHashes;
-    std::string pieces = std::get<0>(d_info["pieces"]->value);
-    tfile.pieceLength = std::get<3>(d_info["piece length"]->value);
+    auto dict = std::get<Bencode::dictionary>(decoder.DecodeElement().value);
+    torrentFile.announce = std::get<Bencode::string>(dict["announce"].value);
+    torrentFile.comment = std::get<Bencode::string>(dict["comment"].value);
 
-    for (int i = 0; i < pieces.size(); i += tfile.pieceLength) {
-        pieceHashes.push_back(pieces.substr(i, tfile.pieceLength));
+    auto info = dict["info"];
+    auto infoDict = std::get<Bencode::dictionary>(info.value);
+    torrentFile.length = std::get<Bencode::number>(infoDict["length"].value);
+    torrentFile.name = std::get<Bencode::string>(infoDict["name"].value);
+    torrentFile.pieceLength = std::get<Bencode::number>(infoDict["piece length"].value);
+
+    std::string pieces = std::get<Bencode::string>(infoDict["pieces"].value);
+    size_t propertyStart = 0;
+    size_t i = pieces.size();
+    while(i > 0) {
+        if (i < 20) {
+            torrentFile.pieceHashes.push_back(pieces.substr(propertyStart, i));
+            propertyStart += i;
+        }
+        else {
+            torrentFile.pieceHashes.push_back(pieces.substr(propertyStart, 20));
+            propertyStart += 20;
+        }
+        i -= 20;
     }
 
-    tfile.pieceHashes = pieceHashes;
-    size_t info_len = Bencode::info_end - Bencode::info_start + 1;
-    char info[info_len];
-    unsigned char hbuf[SHA_DIGEST_LENGTH];
+    Bencode::Encoder encoder;
 
-    tifstream.open(filename);
-   
-    tifstream.unsetf(std::ios_base::skipws);
-    tifstream.seekg(Bencode::info_start - 1);
-    tifstream.read(info, info_len);
+    std::string infoString = encoder.EncodeElement(info);
 
-    tifstream.close();
+    torrentFile.infoHash = CalculateSHA1(infoString);
 
-    SHA1(reinterpret_cast<unsigned char const* >(info), info_len, hbuf);
-
-    std::string info_hash(reinterpret_cast< char const* >(hbuf), SHA_DIGEST_LENGTH);
-    tfile.infoHash = info_hash;
-
-    // delete root;
-
-    return tfile;
+    return torrentFile;
 }
