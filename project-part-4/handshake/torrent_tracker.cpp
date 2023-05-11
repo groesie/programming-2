4fd09f9c11d553f21ca027f6ef90813f3cadc52b
mthgradr <bolatusd@yandex.ru>
2023-05-11 18:32:09 +0300

    Revert "small fixes"
    This reverts commit 31626e4fc822ac8134b8e2237fd49e4455924692.

--- project-part-4/handshake/torrent_tracker.cpp
+++ project-part-4/handshake/torrent_tracker.cpp
@@ -1,53 +1,63 @@
 #include "torrent_tracker.h"
 #include "bencode.h"
 #include "byte_tools.h"
+
 #include <cpr/cpr.h>
-
-TorrentTracker::TorrentTracker(const std::string &url)
-        : url_(url) {}
-
-void TorrentTracker::UpdatePeers(const TorrentFile &tf, std::string peerId, int port) {
-    cpr::Response response = cpr::Get(
-            cpr::Url{url_},
-            cpr::Parameters {
-                    {"info_hash", tf.infoHash},
-                    {"peer_id", peerId},
-                    {"port", std::to_string(port)},
-                    {"uploaded", std::to_string(0)},
-                    {"downloaded", std::to_string(0)},
-                    {"left", std::to_string(tf.length)},
-                    {"compact", std::to_string(1)}
-            },
-            cpr::Timeout{20000}
-    );
-
-    if (response.status_code == 200) {
-        std::string responseText = response.text;
-        if (responseText.find("failure reason") == std::string::npos) {
-            Bencode::Decoder decoder(responseText);
-            auto dict = std::get<Bencode::dictionary>(decoder.DecodeElement().value);
-            std::string_view peers(std::get<Bencode::string>(dict["peers"].value));
-
-            size_t i = peers.size();
-            while (i > 0) {
-                unsigned int ip = BytesToInt(peers);
-
-                int peerPort = static_cast<int>(static_cast<unsigned char>(peers[4]) << 8 |
-                                                static_cast<unsigned char>(peers[5]));
-
-                peers_.push_back({std::to_string(ip >> 24) + "." +
-                                  std::to_string((ip << 8) >> 24) + "." +
-                                  std::to_string((ip << 16) >> 24) + "." +
-                                  std::to_string(((ip << 24) >> 24)), peerPort});
-                peers.remove_prefix(6);
-                i -= 6;
-            }
-        } else {
-            std::cerr << "Server responded '" << responseText << "'" << std::endl;
-        }
-    }
-}
+#include <memory>
 
 const std::vector<Peer> &TorrentTracker::GetPeers() const {
     return peers_;
+}
+
+TorrentTracker::TorrentTracker(const std::string& url) : url_(url) { };
+
+    
+void TorrentTracker::UpdatePeers(const TorrentFile& tf, std::string peerId, int port) {
+    cpr::Response res = cpr::Get(
+        cpr::Url{url_},
+        cpr::Parameters {
+                {"info_hash", tf.infoHash},
+                {"peer_id", peerId},
+                {"port", std::to_string(port)},
+                {"uploaded", std::to_string(0)},
+                {"downloaded", std::to_string(0)},
+                {"left", std::to_string(tf.length)},
+                {"compact", std::to_string(1)}
+        },
+        cpr::Timeout{20000}
+    );
+    std::string tdata(std::move(res.text));
+
+
+    std::stringstream tstream(tdata);
+    tstream.unsetf(std::ios_base::skipws);
+
+    std::shared_ptr<Bencode::TorElement> root = Bencode::getRoot(tstream);
+
+    std::unordered_map<std::string, std::shared_ptr<Bencode::TorElement>> d = std::get<1>(root->value);
+    std::stringstream peerstream(std::get<0>(d["peers"]->value));
+    peerstream.unsetf(std::ios_base::skipws);
+
+    char cur_byte;
+    while (peerstream >> cur_byte) {
+        Peer peer;
+    
+        std::string ip[4];
+        ip[0] = std::to_string((int)(unsigned char)cur_byte);
+        for (int chunk_i = 1; chunk_i < 4; ++chunk_i) {
+            peerstream >> cur_byte;
+            ip[chunk_i] = std::to_string((int)(unsigned char)cur_byte);
+        }
+        peer.ip = ip[0] + "." + ip[1] + "." + ip[2] + "." + ip[3];
+
+        int port;
+        peerstream >> cur_byte;
+        port = (int)(unsigned char)cur_byte * 256;
+        peerstream >> cur_byte;
+        port += (int)(unsigned char)cur_byte;
+        peer.port = port;
+
+        peers_.emplace_back(std::move(peer));
+    }
+    // delete root;
 }
