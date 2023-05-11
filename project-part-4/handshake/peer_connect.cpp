31626e4fc822ac8134b8e2237fd49e4455924692
mthgradr <bolatusd@yandex.ru>
2023-05-11 18:16:34 +0300

    small fixes

--- project-part-4/handshake/peer_connect.cpp
+++ project-part-4/handshake/peer_connect.cpp
@@ -2,13 +2,34 @@
 #include "peer_connect.h"
 #include "message.h"
 #include <iostream>
-#include <sstream>
 #include <utility>
-#include <cassert>
-#include <stdexcept>
 
 using namespace std::chrono_literals;
 
+PeerPiecesAvailability::PeerPiecesAvailability() = default;
+
+PeerPiecesAvailability::PeerPiecesAvailability(std::string bitfield)
+    : bitfield_(std::move(bitfield))
+    {}
+
+bool PeerPiecesAvailability::IsPieceAvailable(size_t pieceIndex) const {
+    return ((bitfield_[pieceIndex / 8] >> (7 - pieceIndex % 8) & 1)) != 0;
+}
+
+void PeerPiecesAvailability::SetPieceAvailability(size_t pieceIndex) {
+    bitfield_[pieceIndex / 8] = char(bitfield_[pieceIndex / 8] | (1 << (7 - pieceIndex % 8)));
+}
+
+size_t PeerPiecesAvailability::Size() const {
+    return bitfield_.size() * 8;
+}
+
+PeerConnect::PeerConnect(const Peer &peer, const TorrentFile &tf, std::string selfPeerId)
+        : tf_(tf),
+          socket_(peer.ip, peer.port, 1000ms, 1000ms),
+          selfPeerId_(std::move(selfPeerId)),
+          choked_(true),
+          terminated_(false) {}
 
 void PeerConnect::Run() {
     while (!terminated_) {
@@ -22,6 +43,22 @@ void PeerConnect::Run() {
     }
 }
 
+void PeerConnect::PerformHandshake() {
+    socket_.EstablishConnection();
+    std::string pstr = "BitTorrent protocol";
+    std::string handshake;
+    handshake += (char) pstr.length();
+    handshake += pstr;
+    for (int i = 0; i < 8; ++i)
+        handshake += '\0';
+    handshake += tf_.infoHash;
+    handshake += selfPeerId_;
+    socket_.SendData(handshake);
+    std::string response = socket_.ReceiveData(handshake.length());
+    std::string infoHashFromPeer = response.substr(28, 20);
+    if (infoHashFromPeer != tf_.infoHash)
+        throw std::runtime_error("Info hashes don't same!");
+}
 
 bool PeerConnect::EstablishConnection() {
     try {
@@ -29,13 +66,27 @@ bool PeerConnect::EstablishConnection() {
         ReceiveBitfield();
         SendInterested();
         return true;
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         std::cerr << "Failed to establish connection with peer " << socket_.GetIp() << ":" <<
-            socket_.GetPort() << " -- " << e.what() << std::endl;
+                  socket_.GetPort() << " -- " << e.what() << std::endl;
         return false;
     }
 }
 
+void PeerConnect::ReceiveBitfield() {
+    const std::string response = socket_.ReceiveData();
+    Message message = Message::Parse(response);
+    if (message.id == MessageId::Unchoke)
+        choked_ = false;
+    else if (message.id != MessageId::BitField)
+        throw std::runtime_error("Cannot receive bitfield!");
+    piecesAvailability_ = PeerPiecesAvailability(message.payload);
+}
+
+void PeerConnect::SendInterested() {
+    std::string interested = Message::Init(MessageId::Interested, "").ToString();
+    socket_.SendData(interested);
+}
 
 void PeerConnect::Terminate() {
     std::cerr << "Terminate" << std::endl;
@@ -49,65 +100,4 @@ void PeerConnect::MainLoop() {
      */
     std::cout << "Dummy main loop" << std::endl;
     Terminate();
-}
-
-PeerPiecesAvailability::PeerPiecesAvailability() {
-    // по умолчанию у пира нет никаких частей файла
-    bitfield_ = "";
-}
-
-PeerPiecesAvailability::PeerPiecesAvailability(std::string bitfield) 
-    : bitfield_(std::move(bitfield)) {}
-
-bool PeerPiecesAvailability::IsPieceAvailable(size_t pieceIndex) const {
-    return (bitfield_[pieceIndex / 8] & (1 << (7 - pieceIndex % 8)) & 1);
-}
-
-void PeerPiecesAvailability::SetPieceAvailability(size_t pieceIndex) {
-    bitfield_[pieceIndex / 8] |= 1 << (7 - pieceIndex % 8);
-}
-
-size_t PeerPiecesAvailability::Size() const {
-    return bitfield_.size() * 8;
-}
-
-PeerConnect::PeerConnect(const Peer& peer, const TorrentFile& tf, std::string selfPeerId)
-    : tf_(tf), socket_(peer.ip, peer.port, 2s, 2s), selfPeerId_(std::move(selfPeerId)), terminated_(false), choked_(true) { }
-
-void PeerConnect::PerformHandshake() {
-    socket_.EstablishConnection();
-    std::string handshake = "\x13" "BitTorrent protocol" "\x00\x00\x00\x00\x00\x00\x00\x00" + tf_.infoHash + selfPeerId_;
-
-    socket_.SendData(handshake);
-    // socket_.EstablishConnection();
-    std::string response = socket_.ReceiveData(68);  // размер handshake сообщения
-    std::cout << response << std::endl;
-    if (response.substr(0, 28) != "\x13" "BitTorrent protocol" "\x00\x00\x00\x00\x00\x00\x00\x00" ||
-        response.substr(28, 20) != tf_.infoHash) {
-        throw std::runtime_error("Invalid handshake response");
-    }
-    peerId_ = response.substr(48, 20);
-} 
-
-
-void PeerConnect::ReceiveBitfield() {
-    std::string response = socket_.ReceiveData();
-    int messageLength = BytesToInt(response.substr(0, 4));
-    uint8_t messageId = response[4];
-    if (messageId == 5) {
-        piecesAvailability_ = PeerPiecesAvailability(response.substr(1));
-    } else if (messageId == 1) {
-        choked_ = false;
-    } else {
-        throw std::runtime_error("Expected Bitfield or Unchoke message");
-    }
-}
-
-void PeerConnect::SendInterested() {
-    Message ms;
-
-    ms = ms.Init(MessageId::Interested, std::string());
-    std::string data = ms.ToString();
-
-    socket_.SendData(data);
 }
