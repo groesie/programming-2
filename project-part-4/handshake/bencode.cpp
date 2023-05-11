23d0cf16c9163f5321a0edc1459395162a554d35
mthgradr <bolatusd@yandex.ru>
2023-05-11 21:24:38 +0300

    fixed

--- project-part-4/handshake/bencode.cpp
+++ project-part-4/handshake/bencode.cpp
@@ -1,109 +1,136 @@
 #include "bencode.h"
 
 namespace Bencode {
-    size_t info_start = -1, info_end = -1;
-    std::string readArr(std::stringstream &tstream) {
-        std::string res;
-        tstream.seekg(-1, std::ios::cur);
-        char cur;
-        tstream >> cur;
-        int len = 0;
-        while(cur != ':') {
-            len = len * 10 + cur - '0';
-            tstream >> cur;
+    Decoder::Decoder(const std::string &torrent_string)
+            : torrent_string(torrent_string)
+    {}
+
+    element Decoder::DecodeElement() {
+        switch(torrent_string[0]) {
+            case 'i': {
+                auto num = DecodeNumber();
+                return element({ .value = num });
+            }
+            case 'l': {
+                auto arr = DecodeList();
+                return element({ .value = arr });
+            }
+            case 'd': {
+                auto dict = DecodeDictionary();
+                return element({ .value = dict });
+            }
+            default: {
+                auto str = DecodeString();
+                return element({ .value = str });
+            }
         }
-        for (int i = 0; i < len; ++i) {
-            tstream >> cur;
-            res.push_back(cur);
-        }
-        return std::move(res);
     }
 
-    uint64_t readInt(std::stringstream &tstream) {
-        char cur;
-        tstream >> cur;
-        uint64_t res = 0;
-        while(cur != 'e') {
-            res = res * 10 + cur - '0';
-            tstream >> cur;
-        }
+    number Decoder::DecodeNumber() {
+        assert(torrent_string[0] == 'i');
+        torrent_string.remove_prefix(1);
+
+        number res{};
+        auto [ptr, errc] = std::from_chars(torrent_string.cbegin(), torrent_string.cend(), res);
+
+        if (errc == std::errc::invalid_argument)
+            std::cerr << "Error occurred while decoding a number: that isn't a number.\n";
+        else if (errc == std::errc::result_out_of_range)
+            std::cerr << "Error occured while decoding a number: this number is larger than an long long.\n";
+
+        assert(*ptr == 'e');
+        torrent_string.remove_prefix(ptr + 1 - torrent_string.cbegin());
+
         return res;
     }
 
-    std::vector<std::shared_ptr<TorElement>> readList(std::stringstream &tstream) {
+    string Decoder::DecodeString() {
+        assert(std::isdigit(torrent_string[0]));
 
-        std::vector<std::shared_ptr<TorElement>> res;
-        char cur;
-        tstream >> cur;
-        while (cur != 'e') {
+        size_t length{};
+        auto [ptr, errc] = std::from_chars(torrent_string.cbegin(), torrent_string.cend(), length);
 
-            std::shared_ptr<TorElement> el = std::shared_ptr<TorElement>(new TorElement);
-            if (cur >= '0' && cur <= '9') {
-                el->value = readArr(tstream);
-            } else if (cur == 'd') {
-                el->value = readDict(tstream);
-            } else if (cur == 'i') {
-                el->value = readInt(tstream);
-            } else if (cur == 'l') {
-                el->value = readList(tstream);
-            }
-            res.push_back(el);
-            tstream >> cur;
-        }
-        return std::move(res);
+        if (errc == std::errc::invalid_argument)
+            std::cerr << "Error occurred while decoding a string length: that isn't a number.\n";
+        else if (errc == std::errc::result_out_of_range)
+            std::cerr << "Error occured while decoding a string length: this number is larger than an size_t.\n";
+
+        torrent_string.remove_prefix(ptr - torrent_string.cbegin());
+
+        assert(*ptr == ':');
+        ++ptr;
+        torrent_string.remove_prefix(1);
+
+        string res{ptr, length};
+        torrent_string.remove_prefix(length);
+
+        return res;
     }
 
-    std::unordered_map<std::string, std::shared_ptr<TorElement>> readDict(std::stringstream &tstream) {
-        std::unordered_map<std::string, std::shared_ptr<TorElement>> res;
-        
-        bool procKey = true;
-        char cur;
-        std::string curKey;
-        while (cur != 'e') {
-            tstream >> cur;
+    list Decoder::DecodeList() {
+        assert(torrent_string[0] == 'l');
+        torrent_string.remove_prefix(1);
 
-            if (procKey && cur >= '0' && cur <= '9') {
-                curKey = readArr(tstream);
-                procKey = false;
-            } else {
-                std::shared_ptr<TorElement> el = std::shared_ptr<TorElement>(new TorElement);
-                if (cur >= '0' && cur <= '9') {
-                    el->value = readArr(tstream);
-                } else if (cur == 'd') {
-                    if (curKey == "info")
-                        info_start = tstream.tellg();
-                    el->value = readDict(tstream);
-                    if (curKey == "info")
-                        info_end = tstream.tellg();
-                } else if (cur == 'i') {
-                    el->value = readInt(tstream);
-                } else if (cur == 'l') {
-                    el->value = readList(tstream);
-                }
-                res[curKey] = el;
-                curKey.clear();
-                procKey = true;
-            }
-        }
-        return std::move(res);
+        list res;
+        while (torrent_string[0] != 'e')
+            res.push_back(DecodeElement());
+
+        assert(torrent_string[0] == 'e');
+        torrent_string.remove_prefix(1);
+
+        return res;
     }
 
-    std::shared_ptr<TorElement> getRoot(std::stringstream &tstream) {
-        char cur;
-        std::shared_ptr<TorElement> root = std::shared_ptr<TorElement>(new TorElement);
+    dictionary Decoder::DecodeDictionary() {
+        assert(torrent_string[0] == 'd');
+        torrent_string.remove_prefix(1);
 
-        while (tstream >> cur) {
-
-            if (cur >= '0' && cur <= '9') {
-                root->value = readArr(tstream);
-            } else if (cur == 'd') {
-                root->value = readDict(tstream);
-            } else if (cur == 'i') {
-                root->value = readInt(tstream);
-            } else if (cur == 'l') {
-                root->value = readList(tstream);
-            }
+        dictionary res;
+        while (torrent_string[0] != 'e') {
+            auto key = DecodeString();
+            res.insert({key, DecodeElement()});
         }
-        return root;
+
+        assert(torrent_string[0] == 'e');
+        torrent_string.remove_prefix(1);
+
+        return res;
+    }
+
+    Encoder::Encoder() = default;
+
+    std::string Encoder::EncodeElement(element &element_) {
+        if (std::holds_alternative<number>(element_.value))
+            return EncodeNumber(std::get<number>(element_.value));
+        else if (std::holds_alternative<string>(element_.value))
+            return EncodeString(std::get<string>(element_.value));
+        else if (std::holds_alternative<list>(element_.value))
+            return EncodeList(std::get<list>(element_.value));
+        else
+            return EncodeDictionary(std::get<dictionary>(element_.value));
+    }
+
+    std::string Encoder::EncodeNumber(number &number_) {
+        return "i" + std::to_string(number_) + "e";
+    }
+
+    std::string Encoder::EncodeString(const string &string_) {
+        return std::to_string(string_.length()) + ":" + string_;
+    }
+
+    std::string Encoder::EncodeList(list &list_) {
+        std::string res = "l";
+        for (auto& element_: list_)
+            res += EncodeElement(element_);
+        return res + "e";
+    }
+
+    std::string Encoder::EncodeDictionary(dictionary &dictionary_) {
+        std::string res = "d";
+        for (auto& [key, val]: dictionary_) {
+            res += EncodeString(key);
+            res += EncodeElement(val);
+        }
+        return res + "e";
     }
 }
--- project-part-4/handshake/bencode.h
+++ project-part-4/handshake/bencode.h
@@ -7,41 +7,57 @@
 #include <list>
 #include <map>
 #include <sstream>
-#include <unordered_map>
-#include <algorithm>
-#include <memory>
+#include <cassert>
+#include <charconv>
+#include <iostream>
 
 namespace Bencode {
-    struct TorElement {
-        std::variant<
-            std::string,
-            std::unordered_map<std::string, std::shared_ptr<TorElement>>,
-            std::vector<std::shared_ptr<TorElement>>,
-            uint64_t
-        > value;
-        // ~TorElement() {
-        //     if (std::holds_alternative<std::vector<TorElement*>>(value)) {
-        //         for (auto i: std::get<std::vector<TorElement*>>(value)) {
-        //             delete i;
-        //         }
-        //     } else if (std::holds_alternative<std::unordered_map<std::string, TorElement*>>(value)) {
-        //         std::unordered_map<std::string, TorElement*> my_map = std::get<std::unordered_map<std::string, TorElement*>>(value);
-        //         std::for_each (my_map.begin(), my_map.end(), [](auto item) -> void
-        //         {
-        //         delete item.second;
-        //         });
-        //     }
-        // }
+
+/*
+ * В это пространство имен рекомендуется вынести функции для работы с данными в формате bencode.
+ * Этот формат используется в .torrent файлах и в протоколе общения с трекером
+ */
+    struct element;
+
+    using number = long long;
+    using string = std::string;
+    using list = std::list<element>;
+    using dictionary = std::map<string, element>;
+
+    struct element {
+        std::variant<number, string, list, dictionary> value;
     };
-    extern size_t info_start, info_end;
 
-    std::unordered_map<std::string, std::shared_ptr<TorElement>> readDict(std::stringstream &tstream);
+    class Decoder {
+    public:
+        explicit Decoder(const std::string& torrent_string);
 
-    std::string readArr(std::stringstream &tstream);
+        element DecodeElement();
+    private:
+        number DecodeNumber();
 
-    uint64_t readInt(std::stringstream &tstream);
+        string DecodeString();
 
-    std::vector<std::shared_ptr<TorElement>> readList(std::stringstream &tstream);
+        list DecodeList();
 
-    std::shared_ptr<TorElement> getRoot(std::stringstream &tstream);
+        dictionary DecodeDictionary();
+
+        std::string_view torrent_string;
+    };
+
+    class Encoder {
+    public:
+        explicit Encoder();
+
+        std::string EncodeElement(element& element_);
+    private:
+        static std::string EncodeNumber(number& number_);
+
+        static std::string EncodeString(const string& string_);
+
+        std::string EncodeList(list& list_);
+
+        std::string EncodeDictionary(dictionary& dictionary_);
+    };
+
 }
