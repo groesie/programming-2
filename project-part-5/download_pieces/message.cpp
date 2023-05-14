a6d96c944c4cdaa953b64ee0fd750c1d717afe43
mthgradr <bolatusd@yandex.ru>
2023-05-14 13:11:51 +0300

    fix message.cpp

--- project-part-5/download_pieces/message.cpp
+++ project-part-5/download_pieces/message.cpp
@@ -1,13 +1,16 @@
 #include "message.h"
+#include "byte_tools.h"
+
 #include <arpa/inet.h>
 #include <sstream>
 
 
 std::string Message::ToString() const {
     std::string messageString;
-    uint32_t length = htonl(static_cast<uint32_t>(messageLength));
-    messageString += reinterpret_cast<const char*>(&length);
-    messageString += reinterpret_cast<const char*>(&id);
+
+    messageString += IntToBytesBigEndian(messageLength);
+    messageString += (char) id;
     messageString += payload;
+
     return messageString;
 }
