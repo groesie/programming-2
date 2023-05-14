7a9c658b82ffa089e47071e55235a165ce035ff2
mthgradr <bolatusd@yandex.ru>
2023-05-14 12:52:16 +0300

    clear code

--- project-part-4/handshake/message.cpp
+++ project-part-4/handshake/message.cpp
@@ -4,10 +4,19 @@
 
 
 std::string Message::ToString() const {
-    std::string messageString;
-    uint32_t length = htonl(static_cast<uint32_t>(messageLength));
-    messageString += reinterpret_cast<const char*>(&length);
-    messageString += reinterpret_cast<const char*>(&id);
-    messageString += payload;
-    return messageString;
+    // std::string messageString;
+    // uint32_t length = htonl(static_cast<uint32_t>(messageLength));
+    // messageString += reinterpret_cast<const char*>(&length);
+    // messageString += reinterpret_cast<const char*>(&id);
+    // messageString += payload;
+    // return messageString;
+
+    std::stringstream buf;
+    char* msgLen = (char*) &messageLength;
+    for (int i = 0; i < 4; ++i)
+        buf << (char) msgLen[3-i];
+    buf << (char) id;
+    buf << payload;
+    return buf.str();
+
 }
