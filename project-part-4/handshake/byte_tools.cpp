2fe18a00609eb3b944d0fe30d70de7caf8df627d
mthgradr <bolatusd@yandex.ru>
2023-05-14 12:02:21 +0300

    clear code

--- project-part-4/handshake/byte_tools.cpp
+++ project-part-4/handshake/byte_tools.cpp
@@ -3,30 +3,17 @@
 #include <openssl/sha.h>
 
 int BytesToInt(std::string_view bytes) {
-    // int res = 0;
-    // for (int i = 0; i < bytes.size(); ++i) {
-    //     res <<= 8;
-    //     res += (int)(unsigned char)bytes[i];
-    // }
-    // return res;
-    return static_cast<int>(static_cast<unsigned char>(bytes[0]) << 24 |
-                            static_cast<unsigned char>(bytes[1]) << 16 |
-                            static_cast<unsigned char>(bytes[2]) << 8 |
-                            static_cast<unsigned char>(bytes[3]));
+    int res = 0;
+    for (int i = 0; i < bytes.size(); ++i) {
+        res <<= 8;
+        res += (int)(unsigned char)bytes[i];
+    }
+    return res;
 }
 
 std::string CalculateSHA1(const std::string& msg) {
-    // unsigned char hash[SHA_DIGEST_LENGTH];
-    // SHA1((unsigned char*)msg.c_str(), msg.length(), hash);
+    unsigned char hash[SHA_DIGEST_LENGTH];
+    SHA1((unsigned char*)msg.c_str(), msg.length(), hash);
 
-    // return std::string((char*)hash, SHA_DIGEST_LENGTH);
-    unsigned char buffer[20];
-    SHA1((unsigned char*)msg.c_str(), msg.length(), buffer);
-
-    std::string hash;
-
-    for (auto &c: buffer)
-        hash += char(c);
-
-    return hash;
+    return std::string((char*)hash, SHA_DIGEST_LENGTH);
 }
