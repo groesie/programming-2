cb85a0d655e33a6c098bd6253ac25057a871ed9c
mthgradr <bolatusd@yandex.ru>
2023-05-23 23:09:56 +0300

    prev part sync

--- project-part-4/handshake/message.cpp
+++ project-part-4/handshake/message.cpp
@@ -7,19 +7,10 @@
 
 std::string Message::ToString() const {
     std::string messageString;
-    // uint32_t length = htonl(static_cast<uint32_t>(messageLength));
-    // messageString += reinterpret_cast<const char*>(&length);
-    // messageString += reinterpret_cast<const char*>(&id);
-    // messageString += payload;
-    // return messageString;
 
-    // std::stringstream buf;
-    // char* msgLen = (char*) &messageLength;
-    // for (int i = 0; i < 4; ++i)
-        // messageString += (char) msgLen[3-i];
     messageString += IntToBytesBigEndian(messageLength);
     messageString += (char) id;
     messageString += payload;
-    return messageString;
 
+    return messageString;
 }
